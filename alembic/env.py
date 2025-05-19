d2fb326f100a15f98175cdc5e510b345b6e5b8b7
Amit Gautam <amitgautam1994@gmail.com>
2025-05-19 17:02:19 +0000

    Added railway for deployment

--- alembic/env.py
+++ alembic/env.py
@@ -14,11 +14,12 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+# Import your models and Base
+from app.db.database import Base
+from app.models import document, user, ingestion  # Import all your models
+
+# Set the target_metadata
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
